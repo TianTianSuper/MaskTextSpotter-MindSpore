dcd1d9bb69174118aca2482a213243e520386bad
TianTianSuper <576221590@qq.com>
2022-10-15 14:19:35 +0800

    resnet50 unittest

--- unittest/test_resnet50.py
+++ unittest/test_resnet50.py
@@ -11,7 +11,7 @@ from src.masktextspotter.resnet50 import ResNetFea, ResidualBlockUsing, _BatchNo
 from src.model_utils.norm import FixedBatchNorm2d
 
 class TestResNet(object):
-    def test_main(self):
+    def test_normal_input(self):
         resnet = ResNetFea(ResidualBlockUsing,
                                   [3, 4, 6, 3],
                                   [64, 256, 512, 1024],
@@ -20,5 +20,44 @@ class TestResNet(object):
         input = Tensor(np.ones((64,3,7,7)))
         output = resnet(input)[-1].asnumpy()
 
+    def test_not_2d(self):
+        with pytest.raises(ValueError):
+            resnet = ResNetFea(ResidualBlockUsing,
+                                    [3, 4, 6, 3],
+                                    [64, 256, 512, 1024, 2048],
+                                    [256, 512, 1024, 2048, 4096],
+                                    False)
+            input = Tensor(np.ones((64,3,7,7)))
+            output = resnet(input)
+    
+    def test_wrong_input_shape(self):
+        with pytest.raises(ValueError):
+            resnet = ResNetFea(ResidualBlockUsing,
+                                    [3, 4, 6, 3],
+                                    [64, 256, 512, 1024],
+                                    [256, 512, 1024, 2048],
+                                    False)
+            input = Tensor(np.ones((3,7,7)))
+            output = resnet(input)
+
+    def test_wrong_Cin(self):
+        with pytest.raises(RuntimeError):
+            resnet = ResNetFea(ResidualBlockUsing,
+                                    [3, 4, 6, 3],
+                                    [64, 256, 512, 1024],
+                                    [256, 512, 1024, 2048],
+                                    False)
+            input = Tensor(np.ones((64,4,7,7)))
+            output = resnet(input)
+
+    def test_other_wh(self):
+        resnet = ResNetFea(ResidualBlockUsing,
+                                [3, 4, 6, 3],
+                                [64, 256, 512, 1024],
+                                [256, 512, 1024, 2048],
+                                False)
+        input = Tensor(np.ones((64,3,128,128)))
+        output = resnet(input)
+
 if __name__ == '__main__':
-    pytest.main(['-vs','--html=unittest/results/report.html'])
+    pytest.main(['-vs','--html=unittest/results/resnet50.html'])
