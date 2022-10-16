f8fc582c7c26fd84627806dbca2a5c7c88fb848b
TianTianSuper <576221590@qq.com>
2022-10-17 02:21:27 +0800

    modified lr

--- src/lr_schedule.py
+++ src/lr_schedule.py
@@ -3,15 +3,10 @@ from bisect import bisect_right
 import math
 
 class WarmUpMultiStepsLR(LearningRateScheduler):
-    def __init__(self, milestones, gamma=0.1, warmup_factor=1.0/3, warmup_iters=500,
+    def __init__(self, config, gamma=0.1, warmup_factor=1.0/3, warmup_iters=500,
                  warmup_method="linear", last_epoch=-1, pow_schedule_mode=False,
                  max_iter=300000, lr_pow=0.9):
-        super(LearningRateScheduler, self).__init__()
-        assert list(milestones) == sorted(milestones), \
-            "All integers in milestones should increase in order, got {}".format(milestones)
-        assert isinstance(warmup_method not in ("constant", "linear")), \
-            "warmup_method is limited to 'constant' or 'linear', got {}".format(milestones)
-        self.milestones = milestones
+
         self.gamma = gamma
         self.warmup_factor = warmup_factor
         self.warmup_iters = warmup_iters
@@ -20,24 +15,23 @@ class WarmUpMultiStepsLR(LearningRateScheduler):
         self.max_iter = max_iter
         self.lr_pow = lr_pow
         self.last_epoch = last_epoch
+        self.base_lr = config.base_lr
         self.lrs = []
+        super(LearningRateScheduler, self).__init__(self.update)
     
-    def construct(self, config):
-        warmup_factor = 1
-        base_lr = config.base_lr
+    def update(self):
+        self.last_epoch += 1
         if self.last_epoch < self.warmup_iters:
             if self.warmup_method == "constant":
                 warmup_factor = self.warmup_factor
             elif self.warmup_method == "linear":
                 alpha = self.last_epoch / self.warmup_iters
                 warmup_factor = self.warmup_factor * (1 - alpha) + alpha
+            else:
+                raise ValueError("Warmup method should be 'constant' or 'linear', got {}".format(self.warmup_factor))
         if self.pow_schedule_mode:
             scale_running_lr = ((1. - float(self.last_epoch) / self.max_iter) ** self.lr_pow)
-            self.lrs.append(base_lr * warmup_factor * scale_running_lr)
+            self.lrs.append(self.base_lr * warmup_factor * scale_running_lr)
         else:
-            self.lrs.append(
-            base_lr
-            * warmup_factor
-            * self.gamma ** bisect_right(self.milestones, self.last_epoch)
-            )
-        return self.lrs
+            self.lrs.append(self.base_lr * warmup_factor * self.gamma ** bisect_right(self.lrs, self.last_epoch))
+        return self.lrs[-1]
